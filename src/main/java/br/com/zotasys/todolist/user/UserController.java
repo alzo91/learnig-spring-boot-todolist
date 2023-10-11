b38d046a370eacd8dea747af829cc1481f07d4cb
Alisson <alzo.zotarelli@gmail.com>
2023-10-10 21:32:04 -0300

    feat: Controller received data and created user using userRepository.

--- src/main/java/br/com/zotasys/todolist/user/UserController.java
+++ src/main/java/br/com/zotasys/todolist/user/UserController.java
@@ -2,6 +2,7 @@ package br.com.zotasys.todolist.user;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @GetMapping("/")
   public String getTodoList(){
     return "Testing ... message";
   }
 
   @PostMapping("/")
-  public ResponseEntity<UserModel> create(@RequestBody UserModel userModel){
-    
-    System.out.println(userModel.toString());
-    return ResponseEntity.status(HttpStatus.CREATED.value()).body(userModel);
+  public ResponseEntity create(@RequestBody UserModel userModel){
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null){
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists!");
+    }
+
+    var savedUser = this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED.value()).body(savedUser);
   }
 }
