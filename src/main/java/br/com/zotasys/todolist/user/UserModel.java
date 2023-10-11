2f77e9bfd9b4e425334d3ae7882110a5efb10784
Alisson <alzo.zotarelli@gmail.com>
2023-10-10 21:05:15 -0300

    feat: Working with projectlombok. It's so easy to handle class with lombok

--- src/main/java/br/com/zotasys/todolist/user/UserModel.java
+++ src/main/java/br/com/zotasys/todolist/user/UserModel.java
@@ -1,27 +1,10 @@
 package br.com.zotasys.todolist.user;
 
+import lombok.Data;
 
+@Data
 public class UserModel {
   private String name;
   private String username;
   private String password;
-
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  public String getUsername() {
-    return username;
-  }
-  public void setUsername(String username) {
-    this.username = username;
-  }
 }
