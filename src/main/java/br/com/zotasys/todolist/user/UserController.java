289226933ff890d684ec907c7300d67fe7e0c098
Alisson <alzo.zotarelli@gmail.com>
2023-10-11 13:21:29 -0300

    feat: Encoded password wit BCrypt

--- src/main/java/br/com/zotasys/todolist/user/UserController.java
+++ src/main/java/br/com/zotasys/todolist/user/UserController.java
@@ -1,7 +1,5 @@
 package br.com.zotasys.todolist.user;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -31,6 +31,9 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists!");
     }
 
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashed);
+    
     var savedUser = this.userRepository.save(userModel);
 
     return ResponseEntity.status(HttpStatus.CREATED.value()).body(savedUser);
