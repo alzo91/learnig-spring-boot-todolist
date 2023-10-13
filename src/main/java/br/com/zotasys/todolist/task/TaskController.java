cd151a9615be3afef2bf0016cca6e4ddcdd7a47e
Alisson <alzo.zotarelli@gmail.com>
2023-10-13 10:25:39 -0300

    feat: Updating one task. List all tasks

--- src/main/java/br/com/zotasys/todolist/task/TaskController.java
+++ src/main/java/br/com/zotasys/todolist/task/TaskController.java
@@ -1,16 +1,22 @@
 package br.com.zotasys.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zotasys.todolist.Utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -25,7 +31,37 @@ public class TaskController {
   public ResponseEntity<String> create(@RequestBody TaskModel taskModel, HttpServletRequest request){
     var idUser  = request.getAttribute("idUser");
     taskModel.setIdUser((UUID)idUser);
+    
+    var currentDate = LocalDateTime.now();
+
+    if (currentDate.isAfter(taskModel.getStartAt())|| currentDate.isAfter(taskModel.getEndAt())){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data 'inicio' ou 'final' deve ser maior que a atual");
+    }
+
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(task.getId().toString());
   }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request){
+    var idUser  = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<String> update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+    var idUser  = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID)idUser);
+    taskModel.setId(id);
+   
+    var task = this.taskRepository.findById(id).orElse(null);
+    
+    Utils.copyNonNullProperties(taskModel,task);
+
+    this.taskRepository.save(task);
+
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body("updated");
+  }
+
 }
