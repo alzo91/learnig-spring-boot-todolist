2d3b8db9a8e3fdd2d64415c2412d511bc92de8a2
Alisson <alzo.zotarelli@gmail.com>
2023-10-14 10:17:41 -0300

    feat: Treating exceptions

--- src/main/java/br/com/zotasys/todolist/task/TaskController.java
+++ src/main/java/br/com/zotasys/todolist/task/TaskController.java
@@ -50,18 +50,27 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<String> update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-    var idUser  = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID)idUser);
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+    var idUser  = (UUID) request.getAttribute("idUser");
+    taskModel.setIdUser(idUser);
     taskModel.setId(id);
    
     var task = this.taskRepository.findById(id).orElse(null);
     
+    if (task == null){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task doesn't find!");
+    }
+
+    System.out.println(idUser.toString() + " ->" + task.getIdUser());
+    if (!task.getIdUser().equals(idUser)){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task doesn't belong to you");
+    }
+
     Utils.copyNonNullProperties(taskModel,task);
 
-    this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
 
-    return ResponseEntity.status(HttpStatus.ACCEPTED).body("updated");
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(taskUpdated);
   }
 
 }
--- src/main/java/br/com/zotasys/todolist/task/TaskModel.java
+++ src/main/java/br/com/zotasys/todolist/task/TaskModel.java
@@ -29,4 +29,12 @@ public class TaskModel {
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  public void setTitle(String title) throws Exception{
+    if (title.length() > 50){
+      throw new Exception("Title length is bigger than 50 charts");
+    }
+
+    this.title = title;
+  }
 }
